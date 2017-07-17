600e8c8953a560ce4b509157f7ff03f22fad4689
Michi Kanda <michi.kanda.0513@gmail.com>
2017-07-18 03:22:49 +0900

    Filter books by shelf type

--- src/App.js
+++ src/App.js
@@ -21,12 +21,45 @@ class BooksApp extends Component {
   }
 
   render() {
+    const { books } = this.state
+    const currentlyReading = books.filter((book)=> book.shelf === 'currentlyReading')
+    const wantToRead = books.filter((book)=> book.shelf === 'wantToRead')
+    const read = books.filter((book)=> book.shelf === 'read')
+
     return (
       <div className="app">
         <Route exact path="/" render={() => (
           <ListBooks
             books={this.state.books}
           />
+          <div className="list-books">
+            <div className="list-books-title">
+              <h1>MyReads</h1>
+            </div>
+
+            <div className="list-books-content">
+              <div>
+                <BookShelf
+                  books={currentlyReading}
+                  shelfTitle='Currently Reading'
+                  onSelectShelf={this.updateShelf}
+                />
+                <BookShelf
+                  books={wantToRead}
+                  shelfTitle='Want to Read'
+                  onSelectShelf={this.updateShelf}
+                />
+                <BookShelf
+                  books={read}
+                  shelfTitle='Read'
+                  onSelectShelf={this.updateShelf}
+                />
+                <div className="open-search">
+                  <Link to="/search">Add a book</Link>
+                </div>
+              </div>
+            </div>
+          </div>
         )} />
         <Route path="/search" component={SearchBooks} />
       </div>
