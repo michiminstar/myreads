d666b910d840230d33cca3bc328f2308d6a9e192
Michi Kanda <michi.kanda.0513@gmail.com>
2017-08-25 14:19:51 +0900

    Import necessary components

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Book from './Book'
 
 class SearchBooks extends Component {
 
